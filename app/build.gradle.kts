4810eee083f01fc229b45e8ac835d73b6230187d
shahrozjames4-svg <shahrozjames4@gmail.com>
2026-09-13 14:51:45 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,13 +30,10 @@ android {
       storePassword = System.getenv("STORE_PASSWORD")
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
-    }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
+    
+}
+
+    
   }
 
   buildTypes {
@@ -44,9 +41,10 @@ android {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
-    }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+      
+    debug {
+}
+
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
